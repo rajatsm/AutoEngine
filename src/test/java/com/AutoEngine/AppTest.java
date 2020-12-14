b08864baf36cc1ea8cf25bab20cb95fb877eab5b
rajatsm <rajatmeharwade@gmail.com>
2020-12-14 21:39:15 +0530

    Initial commit

---
+++ src/test/java/com/AutoEngine/AppTest.java
@@ -0,0 +1,50 @@
+package com.AutoEngine;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+	private WebDriver driver;
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    }
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.edgedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeMethod
+	public void setupTest() {
+        driver = new FirefoxDriver();
+    }
+    /**
+     * Rigourous Test :-)
+     * @throws InterruptedException 
+     */
+    @Test
+	public void testApp() throws InterruptedException
+    {
+    	//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") +"/drivers/chromedriver");
+        //WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
